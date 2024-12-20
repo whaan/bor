74c48299c457f0366b82684b2c9264e7a59be7e1
whaan <wim.dehaan@elemica.com>
2024-12-20 14:16:45 +0100

    Fix issue with shutterspeeds of longer than 1 sec.

--- bor.sh
+++ bor.sh
@@ -30,14 +30,23 @@ apply_exif_annotation()
         fnumber1=`echo $fnumber | cut -d/ -f1`
         fnumber2=`echo $fnumber | cut -d/ -f2`
         fnumber=`echo "scale=1; $fnumber1/$fnumber2" | bc`
+        decho "exptime RAW: $exptime"
         exptime1=`echo $exptime | cut -d/ -f1`
         exptime2=`echo $exptime | cut -d/ -f2`
-        exptime=`echo "scale=0; $exptime2/$exptime1" | bc`
+        if [[ $exptime1 -ge $exptime2 ]]; then
+          # shutterspeed >= 1 sec
+          exptime=`echo "scale=1; $exptime" | bc`
+          exptime=`echo "$exptime sec"`
+        else
+          # shutterspeed < 1 sec
+          exptime=`echo "scale=0; $exptime2/$exptime1" | bc`
+          exptime=`echo "1/$exptime sec"`
+        fi
+        decho "exptime for printing: $exptime"
         fl1=`echo $focallength | cut -d/ -f1`
         fl2=`echo $focallength | cut -d/ -f2`
         focallength=`echo "scale=0; $fl1/$fl2" | bc`
-        exiftext=`echo "$cameramodel $focallength, F$fnumber, 1/$exptime, ISO $isospeed"`
-
+        exiftext=`echo "$cameramodel $focallength, $fnumber, $exptime, $isospeed"`
         decho "EXIF: $exiftext"
 
         EXIF_FORMAT=""
@@ -50,10 +59,10 @@ apply_exif_annotation()
             EXIF_FORMAT="${EXIF_FORMAT} ${focallength}mm"
         fi
         if [[ "$EXIF_OPTIONS" == *fnumber* ]]; then
-            EXIF_FORMAT="${EXIF_FORMAT} F/${fnumber}"
+            EXIF_FORMAT="${EXIF_FORMAT} f/${fnumber}"
         fi
         if [[ "$EXIF_OPTIONS" == *exptime* ]]; then
-            EXIF_FORMAT="${EXIF_FORMAT} 1/${exptime}"
+            EXIF_FORMAT="${EXIF_FORMAT} ${exptime}sec"
         fi
         if [[ "$EXIF_OPTIONS" == *isospeed* ]]; then
             EXIF_FORMAT="${EXIF_FORMAT} ISO ${isospeed}"
@@ -91,7 +100,7 @@ if [ $# -eq 0 ]; then
     exit
 fi
 
-while getopts :b:de:f:q:r:t: OPTION
+while getopts :b:de:f:q:r:t:c: OPTION
 do
     case $OPTION in
         b)
@@ -115,6 +124,9 @@ do
             ;;
         t)
             ARGS_CUSTOM_TEXT=$OPTARG
+            ;;
+        c)
+            ARGS_COPYRIGHT=$OPTARG
             ;;
         \?)
             usage
@@ -195,18 +207,28 @@ else
 fi
 
 OFFSET_H=`echo "${BORDER_H} - ${FONT_SIZE} - (${FONT_SIZE} / 10)" | bc`
+OFFSET_T=`echo "${BORDER_H} - ${FONT_SIZE} - (${FONT_SIZE} / 2)" | bc`
 
 decho "Border size ${BORDER_W} x ${BORDER_H}"
+decho "OFFSET_H ${OFFSET_H}"
+decho "OFFSET_T ${OFFSET_T}"
+
+if [ ! -z "$ARGS_COPYRIGHT" -a ! -z "$ARGS_BORDER" ]; then
+    COMMAND="magick ${INPUT_FILE} -gravity SouthEast -pointsize ${FONT_SIZE} -annotate +${BORDER_W}+${OFFSET_H} \"© ${ARGS_COPYRIGHT}\" ${OUTPUT_FILE}"
+    decho "${COMMAND}"
+    eval $COMMAND
+    INPUT_FILE=$OUTPUT_FILE
+fi
 
 if [ ! -z "$ARGS_CUSTOM_TEXT" -a ! -z "$ARGS_BORDER" ]; then
-    COMMAND="magick ${INPUT_FILE} -gravity SouthWest -pointsize ${FONT_SIZE} -annotate +${BORDER_W}+${OFFSET_H} \"${ARGS_CUSTOM_TEXT}\" ${OUTPUT_FILE}"
+    COMMAND="magick ${INPUT_FILE} -gravity North -pointsize ${FONT_SIZE} -annotate +${BORDER_W}+${OFFSET_T} \"${ARGS_CUSTOM_TEXT}\" ${OUTPUT_FILE}"
     decho "${COMMAND}"
     eval $COMMAND
     INPUT_FILE=$OUTPUT_FILE
 fi
 
 if [ ! -z "$ARGS_ANNOTATION_TEXT" -a ! -z "$ARGS_BORDER" ]; then
-    COMMAND="magick ${INPUT_FILE} -gravity SouthEast -pointsize ${FONT_SIZE} -annotate +${BORDER_W}+${OFFSET_H} \"${ARGS_ANNOTATION_TEXT}\" ${OUTPUT_FILE}"
+    COMMAND="magick ${INPUT_FILE} -gravity SouthWest -pointsize ${FONT_SIZE} -annotate +${BORDER_W}+${OFFSET_H} \"${ARGS_ANNOTATION_TEXT}\" ${OUTPUT_FILE}"
     decho "${COMMAND}"
     eval $COMMAND
 fi
